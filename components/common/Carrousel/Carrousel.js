a882cc5ba2d72e3df809cba179e7c1918073df8a
pedroribeiro <ff.pedro@gmail.com>
2022-10-03 10:24:30 -0300

    add tabs per view

--- components/common/Carrousel/Carrousel.js
+++ components/common/Carrousel/Carrousel.js
@@ -1,9 +1,26 @@
 import { Divider } from "@components/layout";
+import { useEffect, useState } from "react";
 import { Navigation } from "swiper";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
+import { useWindowDimensions } from "utils/useWindowDimensions"
+
 export const Carrousel = () => {
+
+  const { width } = useWindowDimensions()
+
+  const [slides, setSlides] = useState(1)
+
+  useEffect(() => {
+    if (width >= 450) {
+      setSlides(2)
+    } else {
+      setSlides(1)
+    }
+
+  }, [slides, setSlides, width])
+
   return (
     <>
       <div className="full-bleed-dark-grey">
@@ -11,7 +28,7 @@ export const Carrousel = () => {
         <Swiper
           modules={[Navigation]}
           spaceBetween={0}
-          slidesPerView={2}
+          slidesPerView={slides}
           navigation
           pagination={{ clickable: true }}
         >
@@ -41,7 +58,7 @@ const Slide1 = () => {
   return (
     <SlideContainer>
       <div className="text-white text-center text-sm font-light sm:text-lg p-5">
-        <p>
+        <p className="text-white text-shadow-lg">
           Desde 2008 atuando apenas com Direito Previdenciário em todo estado do Rio de Janeiro,
           somos advogados especialistas em ações para concessão e revisão de benefícios do INSS.
         </p>
@@ -63,7 +80,7 @@ const Slide3 = () => {
     <div className="bg-[#E5E5E5] flex items-center justify-center h-44 sm:h-80 w-full text-center text-sm font-light sm:text-lg p-5 judgement-bg bg-cover ">
       <a href="#insalubridade">
         <p className="text-white text-shadow-lg">Insalubridade e periculosidade</p>
-        <p className="text-white">-</p>
+        <p className="text-white text-shadow-lg">-</p>
         <p className="text-white text-shadow-lg">Concessão e revisão de aposentadorias especiais</p>
       </a>
     </div>
